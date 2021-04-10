5278d3a9b7f81afbd401c14ab2c2ce3ec7ab0f01
Thanos Prassas <tprassas@gmail.com>
2021-04-10 20:25:09 +0300

    Patients Router finished

--- routes/patientsRouter.js
+++ routes/patientsRouter.js
@@ -42,4 +42,40 @@ patientsRouter.route('/')
             .catch((err) => next(err));
     });
 
+
+patientsRouter.route('/:patientId')
+    .get((req, res, next) => {
+        Patients.findById(req.params.patientId)
+            .then((patient) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(patient);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /patients/' + req.params.patientId);
+    })
+    .put((req, res, next) => {
+        Patients.findByIdAndUpdate(req.params.patientId, {
+            $set: req.body
+        }, { new: true })
+            .then((patient) => {
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'application/json');
+                res.json(patient);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Patients.findByIdAndRemove(req.params.patientId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
+
 module.exports = patientsRouter;
