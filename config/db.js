2d3d22a4360bc02373ad2c52f3a5dd91daa61da8
Thanos Prassas <tprassas@gmail.com>
2021-04-16 01:03:50 +0300

    Customize db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
-
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
