3dcc0e550267617d352f3460f864737b4cce29bf
George Kostantinos Dimou <georgedimou8@gmail.com>
2021-04-18 21:38:26 +0300

    hospital

--- src/redux/action/Types.js
+++ src/redux/action/Types.js
@@ -7,3 +7,9 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+///redux 
+export const GET_HOSPITAL = 'GET_HOSPITAL';
+export const ADD_HOSPITAL = 'GET_HOSPITAL';
+export const HOSPITAL_ERROR = 'HOSPITAL_ERROR';
+export const UPDATE_HOSPITAL = 'UPDATE_HOSPITAL';
+
