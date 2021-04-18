d13d3ed34319de2e30cbe30297213d1ee7c365ae
GeorgeKonstantinosDimou <72566530+GeorgeKonstantinosDimou@users.noreply.github.com>
2021-04-19 00:09:28 +0300

    hospital (#6)
    Co-authored-by: Thanos Prassas <37278658+codwriter@users.noreply.github.com>

--- src/redux/action/Types.js
+++ src/redux/action/Types.js
@@ -7,12 +7,13 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+export const GET_HOSPITAL = 'GET_HOSPITAL';
+export const ADD_HOSPITAL = 'GET_HOSPITAL';
+export const HOSPITAL_ERROR = 'HOSPITAL_ERROR';
+export const UPDATE_HOSPITAL = 'UPDATE_HOSPITAL';
 export const ADD_PATIENT = 'ADD_PATIENT';
 export const GET_PATIENT = 'GET_PATIENT';
 export const GET_PATIENTS = 'GET_PATIENTS';
 export const UPDATE_PATIENT = 'UPDATE_PATIENT';
 export const DELETE_PATIENT = 'DELETE_PATIENT';
 export const PATIENT_ERROR = 'PATIENT_ERROR';
-
-
-
