e77b5ebcc1e0143add72a8e427ec7235e1a0745a
Thanos Prassas <37278658+codwriter@users.noreply.github.com>
2021-04-19 14:00:54 +0300

    skgproj-34 Completed Vaccine reducer,action (#7)

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import patientReducer from './patient';
+import vaccineReducer from './vaccine';
 
 export default combineReducers({
     alert,
     auth,
-    patientReducer
+    patientReducer,
+    vaccineReducer
 });
---
+++ src/redux/reducers/vaccine.js
@@ -0,0 +1,64 @@
+import {
+    GET_VACCINES,
+    GET_VACCINE,
+    ADD_VACCINE,
+    UPDATE_VACCINE,
+    REMOVE_VACCINE,
+    VACCINE_ERROR
+} from '../action/types';
+
+const initialState = {
+    vaccines: [],
+    vaccine: null,
+    loading: true,
+    error: {}
+};
+
+function vaccineReducer(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case GET_VACCINES:
+            return {
+                ...state,
+                vaccines: payload,
+                loading: false
+            };
+        case GET_VACCINE:
+            return {
+                ...state,
+                vaccine: payload,
+                loading: false
+            };
+        case ADD_VACCINE:
+            return {
+                ...state,
+                vaccines: [payload, ...state.vaccines],
+                loading: false
+            };
+        case UPDATE_VACCINE:
+            return {
+                ...state,
+                vaccines: state.vaccines.map((vaccine) =>
+                    vaccine._id === payload.id ? { ...vaccine, vaccine: payload.vaccine } : vaccine
+                ),
+                loading: false
+
+            };
+        case REMOVE_VACCINE:
+            return {
+                ...state,
+                vaccines: state.vaccines.filter((vaccine) => vaccine._id !== payload),
+                loading: false
+            };
+        case VACCINE_ERROR:
+            return {
+                ...state,
+                error: payload,
+                loading: false
+            };
+        default:
+            return state;
+    }
+}
+export default vaccineReducer;
