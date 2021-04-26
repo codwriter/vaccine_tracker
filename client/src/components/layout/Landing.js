b8fb2aa963c14cf3309ab8ba262a633cb301cac2
Thanos Prassas <tprassas@gmail.com>
2021-04-26 19:10:37 +0300

    SOme fixes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import VaccineLogo from '../../assets/images/Vaccine-Tracker_logo.png'
-
 const Landing = () => {
     return (
         <>
-
     <section className='landing'>
-      <div className='dark-overlay'>
         <div className='landing-inner'>
           <h1 className='x-large'><img src={VaccineLogo} className='landing-logo' /></h1>
-          <div className="buttons">
+              <div className="buttons">
             <Link to='/register' className='btn btn-primary' >Sign Up</Link>
             <Link to='/login' className='btn btn-light' >Login</Link>
           </div>
         </div>
-      </div>
     </section>
     </>
     );
