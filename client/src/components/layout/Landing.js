030e7b2e41eb23853290840e2fbfda2f21ba2f37
lazogiannissotiris <sotiris.lazogiannis@hotmail.com>
2021-04-27 18:42:46 +0300

    landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,19 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col, Media } from 'reactstrap';
 import VaccineLogo from '../../assets/images/Vaccine-Tracker_logo.png'
+import { TabContent, TabPane, Nav, NavItem, NavLink, Button } from 'reactstrap';
+import classnames from 'classnames';
+import {
+    FormGroup,
+    Label,
+    Input,
+    FormText
+  } from "reactstrap";
+
+
 const Landing = () => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+      if(activeTab !== tab) setActiveTab(tab);}
+
     return (
-        <>
-    <section className='landing'>
-        <div className='landing-inner'>
-          <h1 className='x-large'><img src={VaccineLogo} className='landing-logo' /></h1>
-              <div className="buttons">
-            <Link to='/register' className='btn btn-primary' >Sign Up</Link>
-            <Link to='/login' className='btn btn-light' >Login</Link>
-          </div>
-        </div>
-    </section>
-    </>
+    <Container>
+       <Row>
+        <Col xs="6"><Media object src={VaccineLogo}/></Col>
+        <Col xs="6">
+        <div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}
+          >
+            Login
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}
+          >
+            Register
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="6">
+            <form>
+      <FormGroup>
+        <Label for="exampleEmail">Email address</Label>
+        <Input
+          type="email"
+          name="email"
+          id="exampleEmail"
+          placeholder="Enter email"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          id="examplePassword"
+          placeholder="Password"
+        />
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          Check me out
+          <span className="form-check-sign">
+            <span className="check"></span>
+        </span>
+        </Label>
+      </FormGroup>
+      <Button color="primary" type="submit">
+        Submit
+      </Button>
+     </form>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="6">
+            <form>
+      <FormGroup>
+        <Label for="exampleEmail">Email address</Label>
+        <Input
+          type="email"
+          name="email"
+          id="exampleEmail"
+          placeholder="Enter email"
+        />
+        <FormText color="muted">
+          We'll never share your email with anyone else.
+        </FormText>
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          id="examplePassword"
+          placeholder="Password"
+        />
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          Check me out
+          <span className="form-check-sign">
+            <span className="check"></span>
+        </span>
+        </Label>
+      </FormGroup>
+      <Button color="primary" type="submit">
+        Submit
+      </Button>
+    </form>
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
+    </div>
+        </Col>
+      </Row>
+    </Container>
     );
 }
 
