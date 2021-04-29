867c72a1f5a9bfea75e310e6d8d54bb060885d43
lazogiannissotiris <sotiris.lazogiannis@hotmail.com>
2021-04-29 14:06:28 +0300

    card

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,124 +10,67 @@ import {
     Input,
     FormText
   } from "reactstrap";
-
+import Login from '../auth/Login';
+import Register from '../auth/Register';
+import { Card,  CardHeader, CardFooter, CardBody,
+  CardTitle, CardText } from 'reactstrap';
 
 const Landing = () => {
-    const [activeTab, setActiveTab] = useState('1');
+  const [activeTab, setActiveTab] = useState('1');
 
-    const toggle = tab => {
+  const toggle = tab => {
       if(activeTab !== tab) setActiveTab(tab);}
 
-    return (
+  return (
     <Container>
-       <Row>
-        <Col xs="6"><Media object src={VaccineLogo}/></Col>
-        <Col xs="6">
-        <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            Login
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
-          >
-            Register
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <Row>
-            <Col sm="6">
-            <form>
-      <FormGroup>
-        <Label for="exampleEmail">Email address</Label>
-        <Input
-          type="email"
-          name="email"
-          id="exampleEmail"
-          placeholder="Enter email"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="examplePassword"
-          placeholder="Password"
-        />
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
-          <span className="form-check-sign">
-            <span className="check"></span>
-        </span>
-        </Label>
-      </FormGroup>
-      <Button color="primary" type="submit">
-        Submit
-      </Button>
-     </form>
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="2">
-          <Row>
-            <Col sm="6">
-            <form>
-      <FormGroup>
-        <Label for="exampleEmail">Email address</Label>
-        <Input
-          type="email"
-          name="email"
-          id="exampleEmail"
-          placeholder="Enter email"
-        />
-        <FormText color="muted">
-          We'll never share your email with anyone else.
-        </FormText>
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="examplePassword"
-          placeholder="Password"
-        />
-      </FormGroup>
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
-          <span className="form-check-sign">
-            <span className="check"></span>
-        </span>
-        </Label>
-      </FormGroup>
-      <Button color="primary" type="submit">
-        Submit
-      </Button>
-    </form>
-            </Col>
-          </Row>
-        </TabPane>
-      </TabContent>
-    </div>
+      <Row>
+        <Col sm="4"><Media object src={VaccineLogo}/></Col>
+          
+        <Col xs="8">
+          <div>
+            <Card>
+              <CardHeader>
+            <Nav tabs>
+              <Row>
+                <Col>
+                  <NavItem tag="h3" className="tab_button" >
+                   <NavLink className={classnames({ active: activeTab === '1' })} onClick={() => { toggle('1'); }}>
+                    Login
+                   </NavLink>
+                  </NavItem>
+                </Col>
+              </Row>
+
+              <NavItem tag="h3" className="tab_button" >
+                <NavLink className={classnames({ active: activeTab === '2' })} onClick={() => { toggle('2'); }}>
+                  Register
+                </NavLink>
+              </NavItem >
+            </Nav>
+            </CardHeader>
+            
+            <TabContent activeTab={activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  <Col sm="6">
+                    <Login />
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col sm="6">
+                  <Register />
+                  </Col>
+                </Row>
+              </TabPane>
+            </TabContent>
+            </Card>
+          </div>
         </Col>
       </Row>
     </Container>
-    );
+  );
 }
 
 export default Landing;
